e01327f6e6106309b471a00291cf8a5ffc09f3da
Suarenguen <adrian.suarez.monzon@gmail.com>
2023-03-28 11:56:59 +0100

    models of members, donations, donor, volunteer Done

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router()
 
 
 router.use('/member', require('./member.route'))
+router.use('/donor', require('./donor.route'))
+router.use('/volunteer', require('./volunteer.route'))
+router.use('/donations', require('./donations.route'))
+
 //Here the routes
 
 module.exports = router
--- database/models.js
+++ database/models.js
@@ -1,12 +1,16 @@
 //Add models here, for example:
 //const User = require('./models/user')
 const Member = require('../api/models/member.model')
+const Donor = require('../api/models/donor.model')
+const Volunteer = require('../api/models/volunteer.model')
+const Donations = require('../api/models/donations.model')
+
 
 
 
 function addRelationsToModels() {
 	try {
-        //Add relations here
+        
 		console.log('Relations added to all models')
 	} catch (error) {
 		throw error
