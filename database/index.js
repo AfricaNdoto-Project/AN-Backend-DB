0a33614688b4766e77c2df77d7e79f6a8289f9c2
Suareguen <adrian.suarez.monzon@gmail.com>
2023-05-09 20:15:28 +0100

    General: - Refactor create project and add to volunteer

--- database/index.js
+++ database/index.js
@@ -6,12 +6,13 @@ const connection = new Sequelize(process.env.DATABASE, 'root', 'Lampara.1', {
 	dialect: process.env.DIALECT,
 	port: process.env.DB_PORT,
 	logging: false,
-const connection = new Sequelize(process.env.DIALECT+'://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME,{
-    dialect: process.env.DIALECT,
-    port: process.env.DB_PORT,
-    logging:false
-
 })
+// const connection = new Sequelize(process.env.DIALECT+'://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME,{
+//     dialect: process.env.DIALECT,
+//     port: process.env.DB_PORT,
+//     logging:false
+
+// })
 
 async function checkConnection() {
 	try {
